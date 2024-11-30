1c0ef6fa52b4f5c4154509e6de2aea689adca09a
tatata05 <takumi.takahata@xincere.jp>
2024-11-30 16:19:12 +0900

    first commit

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  // 他と被りそうなのでひとまず5000
+  await app.listen(process.env.PORT ?? 5000);
+}
+bootstrap();
